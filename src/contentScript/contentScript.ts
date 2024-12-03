d04469def0dd36be0d8822155e4fafc0bc29c42f
Yoshihiko Miyaichi <miyaichi@gmail.com>
2024-12-04 03:21:07 +0900

    Refactor content script to streamline connection setup

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -50,10 +50,7 @@ class ContentScript {
 
     try {
       this.logger.debug('Setting up connection', { tabId });
-      this.connectionManager = new ConnectionManager(
-        `content-${tabId}`,
-        this.handleMessage
-      );
+      this.connectionManager = new ConnectionManager(`content-${tabId}`, this.handleMessage);
       this.connectionManager.connect();
       this.logger.debug('Connection established', { tabId });
     } catch (error) {
